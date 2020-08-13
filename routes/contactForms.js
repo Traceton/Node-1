2f8b90a1e1c540cbdcdf55dcc7b4e59c007dcfab
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-08-13 00:16:32 -0400

    finished basic get and create contact form routes

--- routes/contactForms.js
+++ routes/contactForms.js
@@ -2,14 +2,37 @@ const express = require("express");
 const router = express();
 const ContactForm = require("../models/contactForm");
 
+// find contact form by id, middleware function
+let findContactForm = (req, res, next) => {
+  next();
+};
+
 // get all contact forms
-router.get("/", (req, res) => {
-  res.send(`<h1>Contact forms</h1>`);
+router.get("/", async (req, res) => {
+  const contactForms = await ContactForm.find();
+  try {
+    res.status(201).json(contactForms);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 // get single contact form
 router.get("/:id", (req, res) => {});
 // create single contact form
-router.post("/", (req, res) => {});
+router.post("/", async (req, res) => {
+  const newContactForm = await new ContactForm({
+    fullName: req.body.fullName,
+    phone: req.body.phone,
+    email: req.body.email,
+    message: req.body.message,
+  });
+  try {
+    newContactForm.save();
+    res.status(201).json(newContactForm);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 // delete single contact form
 router.delete("/:id", (req, res) => {});
 // update single contact form
