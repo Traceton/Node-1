cb996f1689abc6f5f794ba9f79e4b323329d3f33
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-08-15 15:02:24 -0400

    added access control properties to server.js.

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ database.once("open", () => {
 
 server.use(express.json());
 
+server.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use("/contactForms", contactFormsRouter);
 server.use("/blogPosts", blogPostsRouter);
 server.use("/users", usersRouter);
