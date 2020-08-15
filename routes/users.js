39df165a0d3e94f00d26d0a3bf901b82574a8478
Traceton Timmerman <traceton.timmerman@gmail.com>
2020-08-14 23:08:28 -0400

    user routes finished but not tested.

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,79 @@ const express = require("express");
 const router = express();
 const User = require("../models/user");
 
+// find user middleware function
+let findUser = async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "Could not find user" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  next();
+};
+
 // get all users
-router.get("/", (req, res) => {
-  res.send(`<h1>users</h1>`);
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(201).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 // get single user by id
-router.get("/:id", (req, res) => {});
+router.get("/:id", findUser, (req, res) => {
+  try {
+    res.status(201).json(res.user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 // create a single user
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  let user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+  });
+});
 // delete a single user
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", findUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.status(201).json({ message: "user was deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 // update a single user
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", findUser, async (req, res) => {
+  if (req.body.firstName != null) {
+    res.user.firstName = req.body.firstName;
+  }
+  if (req.body.lastName != null) {
+    res.user.lastName = req.body.lastName;
+  }
+  if (req.body.username != null) {
+    res.user.username = req.body.username;
+  }
+  if (req.body.password != null) {
+    res.user.password = req.body.password;
+  }
+  if (req.body.email != null) {
+    res.user.email = req.body.email;
+  }
+  try {
+    const updatedUser = await res.user.save();
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
